fd67db78981a382031beadc9a86babf11ec4d159
Tommy Harris <tobraha@tilde.team>
2023-04-18 15:01:17 -0600

    [UPD] Update to v1.0.4

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ README = (CURR_DIR / "README.md").read_text()
 
 setuptools.setup(
      name='iris_webhooks_module',
-     version='1.0.3',
+     version='1.0.4',
      packages=['iris_webhooks_module'],
      author="DFIR-IRIS",
      author_email="contact@dfir-iris.org",
